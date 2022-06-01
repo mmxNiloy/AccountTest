c7d4e94a089f879f1d835a2b0e88aca9937a7e7b
Niloy <dev.niloysarkar@gmail.com>
2022-06-01 21:15:08 +0600

    Project code simplified.

--- src/com/mmx/AccountTest.java
+++ src/com/mmx/AccountTest.java
@@ -5,87 +5,35 @@ import java.util.Random;
 public class AccountTest {
 
     public static void main(String[] args) {
-        // Hardcoded data
-        String[] names = {"John Doe", "Jane Doe", "Horatio Nelson", "Ivar Ragnarsson", "Radzig Kobyla"};
-        String[] accountTypes = {"Personal", "Merchant"};
-        Random rd = new Random();
-	    // Experiment 1: Without constructor
-        System.out.println("Experiment 1: Without constructor");
+        // Hardcoded names
+        String[] names = {"Test A", "Test B", "Test C", "Test D", "Test E"};
 
-        System.out.println("Initialization");
+        // [Experiment 1]
         BankAccount[] accounts = new BankAccount[5];
-        for(int i = 0 ; i < accounts.length ; i++) {
+        for(int i = 0 ; i < 5 ; i++) {
             accounts[i] = new BankAccount();
-            accounts[i].initializeAccount(names[i],
-                    makeAccountNumber(names[i]), accountTypes[i & 1],
-                    (float)Math.pow(10, (int)(rd.nextFloat() * 10)) * rd.nextFloat());
-
-            printAccount(accounts[i], i);
-        }
-
-        depositAndWithdrawDemonstration(accounts);
-
-        // [End of Experiment 1]
-
-        // Experiment 2: With constructor
-        System.out.println("\nExperiment 2: With constructor");
-        for(int i = 0 ; i < accounts.length ; i++) {
-            accounts[i] = new BankAccount(
-                    names[i],
-                    makeAccountNumber(names[i]), accountTypes[i & 1],
-                    (float)Math.pow(10, (int)(rd.nextFloat() * 10)) * rd.nextFloat()
+            accounts[i].setAccountInformation(
+                    names[i], names[i], names[i],
+                    (float)(i * (i + 1) * (i + 2)) / 2.0f
             );
 
-            printAccount(accounts[i], i);
+            accounts[i].printNameAndBalance();
+            accounts[i].deposit(3 * (i + 1));
+            accounts[i].withdraw(5 * i);
+            accounts[i].printNameAndBalance();
         }
 
-        depositAndWithdrawDemonstration(accounts);
-        // [End of Experiment 2]
-    }
+        // [Experiment 2]
+        for(int i = 0 ; i < 5 ; i++) {
+            accounts[i] = new BankAccount(
+                    names[i], names[i], names[i],
+                    (float)(4 + 3 * i) / (float) (5 - i)
+            );
 
-    private static void depositAndWithdrawDemonstration(BankAccount[] accounts) {
-        Random rd = new Random();
-
-        System.out.println();
-        System.out.println("Before and after depositing");
-        for (BankAccount account : accounts) {
-            System.out.println("Before depositing");
-            System.out.println(account);
-            account.depositBalance((float) Math.pow(10, ((int) ((rd.nextFloat() * 10)) % 5) + 1) * rd.nextFloat());
-            System.out.println("After depositing");
-            System.out.println(account);
-            System.out.println();
+            accounts[i].printNameAndBalance();
+            accounts[i].deposit(3 * (i + 1));
+            accounts[i].withdraw(5 * i);
+            accounts[i].printNameAndBalance();
         }
-
-        System.out.println();
-        System.out.println("Before and after a withdrawal");
-        for (BankAccount account : accounts) {
-            System.out.println("Before withdrawal");
-            System.out.println(account);
-            account.withdrawFromBalance((float) Math.pow(10, (int) (rd.nextFloat() * 10)) * rd.nextFloat());
-            System.out.println("After withdrawal");
-            System.out.println(account);
-            System.out.println();
-        }
-    }
-
-    private static Long makeAccountNumber(String name) {
-        Long ans = 0L;
-        for(int i = 0 ; i < name.length() ; i++) {
-            ans += Character.getNumericValue(name.charAt(i)) % 23;
-            ans *= 10;
-        }
-        return ans;
-    }
-
-    private static void printAccount(BankAccount account, int index) {
-        System.out.printf(
-                "BankAccount #%d: { name: %s, accNumber: %d, accType: %s, balance: $%.2f }%n",
-                index + 1,
-                account.getDeveloperName(),
-                account.getAccountNumber(),
-                account.getAccountType(),
-                account.getBalance()
-        );
     }
 }
--- src/com/mmx/BankAccount.java
+++ src/com/mmx/BankAccount.java
@@ -2,82 +2,43 @@ package com.mmx;
 
 public class BankAccount {
     // Constructors
-
-    public BankAccount(String developerName, Long accountNumber,
-                       String accountType, Float balance) {
-        this.developerName = developerName;
-        this.accountNumber = accountNumber;
-        this.accountType = accountType;
-        this.balance = balance;
-    }
-
-    public BankAccount() {
-
-    }
-
-    // Getters and setters
-
-    public String getDeveloperName() {
-        return developerName;
-    }
-
-    public void setDeveloperName(String developerName) {
-        this.developerName = developerName;
-    }
-
-    public Long getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(Long accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public String getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
-    }
-
-    public Float getBalance() {
-        return balance;
-    }
-
-    public void setBalance(Float balance) {
-        this.balance = balance;
+    BankAccount() {}
+    BankAccount(String developerName,
+                String accountNumber,
+                String accountType,
+                float balance) {
+        setAccountInformation(
+                developerName, accountNumber,
+                accountType, balance
+        );
     }
 
     // Methods
-
-    public void initializeAccount(String developerName, Long accountNumber,
-                                  String accountType, Float balance) {
+    public void setAccountInformation(String developerName,
+                                      String accountNumber,
+                                      String accountType,
+                                      float balance) {
         this.developerName = developerName;
         this.accountNumber = accountNumber;
         this.accountType = accountType;
         this.balance = balance;
     }
 
-    public void depositBalance(Float amount) {
-        balance = balance + amount;
+    public void deposit(float amount) {
+        balance += amount;
     }
 
-    public void withdrawFromBalance(Float amount) {
-        // You cannot withdraw more than your current balance
-        balance = Math.max(0, balance - amount);
+    public void withdraw(float amount) {
+        balance = Math.max(0.0f, balance - amount);
     }
 
-    @Override
-    public String toString() {
-        return String.format("BankAccount: { name: %s, balance: $%.2f }",
-                developerName, balance);
+    public void printNameAndBalance() {
+        System.out.printf("Name = %s, Balance = %.2f\n", developerName, balance);
     }
 
     // Data fields
-
     private String developerName;
-    private Long accountNumber;
+    private String accountNumber;
     private String accountType;
-    private Float balance;
+    private float balance;
 }
